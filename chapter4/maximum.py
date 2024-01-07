fa3b3396f72b3e65ea0e6bd064523e0fd97af1e7
onyx-and-iris <code@onyxandiris.online>
2024-01-07 21:19:47 +0000

    add maximum

---
+++ chapter4/maximum.py
@@ -0,0 +1,19 @@
+import logging
+import math
+import random
+
+highest = math.inf
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def maximum(nums, size):
+    if size == 1:
+        return nums[0]
+    return max(nums[size - 1], maximum(nums, size - 1))
+
+
+randomlist = random.sample(range(10, 300), 5)
+logger.debug(randomlist)
+print(maximum(randomlist, len(randomlist)))
